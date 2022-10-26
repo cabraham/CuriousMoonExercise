a61cd7a644af41418c27d8814f4c0b24aedf4c72
Ciby Abraham <5863494+cabraham@users.noreply.github.com>
2022-10-25 21:50:28 -0400

    Adding events, relational tables, and references

--- scripts/import.sql
+++ scripts/import.sql
@@ -13,16 +13,3 @@ create table import.master_plan(
 	description text
 );
 
-drop table if exists events;
-
-create table events(
-	id serial primary key,
-	time_stamp timestamptz not null,
-	title varchar(500),
-	description text,
-	event_type_id int,
-	spass_type_id int,
-	target_id int,
-	team_id int,
-	request_id int
-);
